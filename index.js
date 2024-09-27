41a7b38eb3adf539e93f29fa8bdcd4713be3f2de
Gregory Chebatyuk <gchebatyu@gmail.com>
2024-09-27 13:38:32 +0300

    changes index.js in master

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ console.log(`hello`)
 
 console.log(`Ветка`)
 
-<<<<<<< HEAD
 const userage = 54;
 
 if (userage >= 18) {
-=======
-const age = 20;
-
-if (age >= 18) {
->>>>>>> feature/user-access
     console.log(`Вы совершеннолетний`)
 } else {
     console.log(`Вы НЕ совершеннолетний`)
