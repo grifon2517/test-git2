4baab068f7881f32634bf5fae6b0afba916dfc4d
Gregory Chebatyuk <gchebatyu@gmail.com>
2024-09-27 13:31:46 +0300

    made changes in master

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log(`hello`)
 
 console.log(`Ветка`)
+
+const userage = 54;
+
+if (userage >= 18) {
+    console.log(`Вы совершеннолетний`)
+} else {
+    console.log(`Вы НЕ совершеннолетний`)
+}
